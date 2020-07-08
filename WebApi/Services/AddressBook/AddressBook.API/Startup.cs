784d447047597db140b9599ba315965a3f903c41
zanuccod <54598430+zanuccod@users.noreply.github.com>
2020-07-08 11:54:12 +0200

    Add Swagger support for debugging and testing

--- WebApi/Services/AddressBook/AddressBook.API/Startup.cs
+++ WebApi/Services/AddressBook/AddressBook.API/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AddressBook.API.Models;
 using AddressBook.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace AddressBook.API
 {
@@ -28,6 +22,12 @@ namespace AddressBook.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "AddressBook.API" });
+            });
+            services.AddSwaggerGenNewtonsoftSupport();
 
             services.AddSingleton<IContactService, ContactService>();
 
@@ -53,6 +53,12 @@ namespace AddressBook.API
         {
             if (env.IsDevelopment())
             {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AddressBook.API");
+                });
+
                 app.UseDeveloperExceptionPage();
             }
 
@@ -64,6 +70,8 @@ namespace AddressBook.API
             {
                 endpoints.MapControllers();
             });
+
+
         }
     }
 }
